50b0b166bb0383d48e55f1f30a22c4efac3d9139
bleumi85 <jens.bleumer@gmail.com>
2022-08-30 13:16:47 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react";
+
+const App: React.FC = (): JSX.Element => {
+
+
+    return (
+        <Box color='purple.500'>Hello World</Box>
+    )
+}
+
+export default App;
